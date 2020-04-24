b0ed2ffaba861fe4ad73355aea73d44a6151ed6a
felipe.palacios <felipe.palacios@pedidosya.com>
2020-04-24 13:51:42 -0300

    first commit

---
+++ spring-billing-liquidation-service/src/main/java/com/peya/billing/aws/SpringBillingLiquidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peya.billing.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBillingLiquidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBillingLiquidationServiceApplication.class, args);
+	}
+
+}
---
+++ spring-billing-liquidation-service/src/test/java/com/peya/billing/aws/SpringBillingLiquidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peya.billing.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBillingLiquidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
